e3b250a9577ff084809ec44f56f75f663450f8af
Pablo Cuevas <cuevasgpablo@gmail.com>
2024-02-13 17:03:01 -0300

    fix: paragraph inside paragraph

--- layouts/default.vue
+++ layouts/default.vue
@@ -23,27 +23,39 @@ const username = ref('pablo-cg');
 
 <template>
   <header class="w-full sticky top-0 inset-x-0">
-    <div class="container mx-auto py-7 px-4 md:px-0 flex items-center justify-between">
+    <div
+      class="container mx-auto py-7 px-4 md:px-0 flex items-center justify-between"
+    >
       <section class="font-bold text-3xl leading-none font-montserrat">
         <NuxtLink to="/" class="gradient-text text-transparent">
           Shorthis
         </NuxtLink>
       </section>
       <section class="flex gap-2">
-        <NuxtLink v-if="!authenticated" to="/auth"
-          class="px-2 py-1 rounded-lg text-white hover:text-light transition duration-300 border border-transparent">
+        <NuxtLink
+          v-if="!authenticated"
+          to="/auth"
+          class="px-2 py-1 rounded-lg text-white hover:text-light transition duration-300 border border-transparent"
+        >
           Sign in
         </NuxtLink>
-        <button v-else
-          class="px-2 py-1 rounded-lg text-white hover:text-light transition duration-300 border border-transparent active:border-white">
+        <button
+          v-else
+          class="px-2 py-1 rounded-lg text-white hover:text-light transition duration-300 border border-transparent active:border-white"
+        >
           <span class="i-lucide:at-sign" /> {{ username }}
         </button>
         <button
-          class="flex justify-center items-center px-2 py-1 rounded-lg transition duration-300 hover:text-white hover:scale-115">
+          class="flex justify-center items-center px-2 py-1 rounded-lg transition duration-300 hover:text-white hover:scale-115"
+        >
           <span class="i-lucide:command" />
         </button>
-        <NuxtLink to="https://github.com/pablo-cg/shorthis" external target="_blank"
-          class="flex justify-center items-center px-2 py-1 rounded-lg transition duration-300 hover:text-white hover:scale-115">
+        <NuxtLink
+          to="https://github.com/pablo-cg/shorthis"
+          external
+          target="_blank"
+          class="flex justify-center items-center px-2 py-1 rounded-lg transition duration-300 hover:text-white hover:scale-115"
+        >
           <span class="i-lucide:github" />
         </NuxtLink>
       </section>
@@ -53,28 +65,50 @@ const username = ref('pablo-cg');
   <NuxtPage />
 
   <footer class="w-full fixed bottom-0 inset-x-0">
-    <div class="container mx-auto py-4 px-4 md:px-0 flex items-center justify-between">
+    <div
+      class="container mx-auto py-4 px-4 md:px-0 flex items-center justify-between"
+    >
       <section class="text-sm text-light/70">
         <p>
           ⚡Made by
-          <NuxtLink external target="_blank" to="https://github.com/pablo-cg" class="hover:text-white">
+          <NuxtLink
+            external
+            target="_blank"
+            to="https://github.com/pablo-cg"
+            class="hover:text-white"
+          >
             @pablo-cg
           </NuxtLink>
           using
-          <NuxtLink external target="_blank" to="https://nuxt.com" class="hover:text-nuxt-green">
+          <NuxtLink
+            external
+            target="_blank"
+            to="https://nuxt.com"
+            class="hover:text-nuxt-green"
+          >
             Nuxt 3
             <span class="i-lucide:external-link text-xs" />
           </NuxtLink>
-        <p class="text-xs">
-          Based on
-          <NuxtLink external target="_blank" to="https://slug.vercel.app/" class="hover:text-white">
-            slug
-          </NuxtLink>
-          by
-          <NuxtLink external target="_blank" to="https://github.com/pheralb" class="hover:text-white">
-            @pheralb
-          </NuxtLink>
-        </p>
+          <span class="text-xs block">
+            Based on
+            <NuxtLink
+              external
+              target="_blank"
+              to="https://slug.vercel.app/"
+              class="hover:text-white"
+            >
+              slug
+            </NuxtLink>
+            by
+            <NuxtLink
+              external
+              target="_blank"
+              to="https://github.com/pheralb"
+              class="hover:text-white"
+            >
+              @pheralb
+            </NuxtLink>
+          </span>
         </p>
       </section>
     </div>
