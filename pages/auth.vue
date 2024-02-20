0d85457353376c72c7e2a54c62b6d659880acbcf
Pablo Cuevas <cuevasgpablo@gmail.com>
2024-02-20 14:39:41 -0300

    dev: navigation to dashboard from signin button

--- pages/auth.vue
+++ pages/auth.vue
@@ -3,12 +3,15 @@
 <template>
   <main class="container mx-auto px-4 md:px-0">
     <section class="mt-10 text-center">
-      <h1 class="font-semibold font-montserrat text-3xl text-white mb-2 flex items-center justify-center gap-3">
+      <h1
+        class="font-semibold font-montserrat text-3xl text-white mb-2 flex items-center justify-center gap-3"
+      >
         <IconWavingHand /> Welcome
       </h1>
     </section>
     <section class="flex gap-2 justify-center mt-5 font-sans">
       <button
+        @click="navigateTo('/dashboard')"
         class="flex gap-2 items-center px-2 py-1 rounded-md transition duration-300 text-white bg-zinc-800 hover:text-light"
       >
         <span class="i-lucide:github" />
