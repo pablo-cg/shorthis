fa33148ada990a07f3de28d4389c3356c4a68016
Pablo Cuevas <cuevasgpablo@gmail.com>
2024-02-20 14:18:22 -0300

    conf: headlessui module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@unocss/nuxt', '@vueuse/nuxt'],
-  css: ['@unocss/reset/tailwind.css', '~/assets/css/main.css'],	
+  modules: ['@unocss/nuxt', '@vueuse/nuxt', "nuxt-headlessui"],
+  css: ['@unocss/reset/tailwind.css', '~/assets/css/main.css'],
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
   },
