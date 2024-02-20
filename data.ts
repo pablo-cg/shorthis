d3a576728a1bd12e668278f7671606d630b78c5c
Pablo Cuevas <cuevasgpablo@gmail.com>
2024-02-20 14:10:18 -0300

    add: mock data

---
+++ data.ts
@@ -0,0 +1,92 @@
+export const links = [
+  {
+    slug: 'google',
+    url: 'https://www.google.com',
+    description: 'A popular search engine.',
+  },
+  {
+    slug: 'github',
+    url: 'https://github.com',
+    description: 'Platform for hosting and collaborating on Git repos.',
+  },
+  {
+    slug: 'twitter',
+    url: 'https://twitter.com',
+    description: 'Social media platform for microblogging and netw.',
+  },
+  {
+    slug: 'reddit',
+    url: 'https://www.reddit.com',
+    description: 'Social news aggregation, web content rating, and.',
+  },
+  {
+    slug: 'stackoverflow',
+    url: 'https://stackoverflow.com',
+    description: 'Question and answer website for programmers.',
+  },
+  {
+    slug: 'wikipedia',
+    url: 'https://www.wikipedia.org',
+    description: 'Free online encyclopedia.',
+  },
+  {
+    slug: 'youtube',
+    url: 'https://www.youtube.com',
+    description: 'Video-sharing platform.',
+  },
+  {
+    slug: 'amazon',
+    url: 'https://www.amazon.com',
+    description: 'Online marketplace and cloud computing platform.',
+  },
+  {
+    slug: 'netflix',
+    url: 'https://www.netflix.com',
+    description: 'Streaming service for movies and TV shows.',
+  },
+  {
+    slug: 'instagram',
+    url: 'https://www.instagram.com',
+    description: 'Social media platform for sharing photos and vi.',
+  },
+  {
+    slug: 'linkedin',
+    url: 'https://www.linkedin.com',
+    description: 'Professional networking platform.',
+  },
+  {
+    slug: 'ebay',
+    url: 'https://www.ebay.com',
+    description: 'Online auction and shopping website.',
+  },
+  {
+    slug: 'apple',
+    url: 'https://www.apple.com',
+    description: 'Technology company designing consumer electronics.',
+  },
+  {
+    slug: 'microsoft',
+    url: 'https://www.microsoft.com',
+    description: 'Multinational technology corporation.',
+  },
+  {
+    slug: 'spotify',
+    url: 'https://www.spotify.com',
+    description: 'Digital music streaming service.',
+  },
+  {
+    slug: 'hackernews',
+    url: 'https://news.ycombinator.com',
+    description: 'Social news website focusing on computer science.',
+  },
+  {
+    slug: 'coursera',
+    url: 'https://www.coursera.org',
+    description: 'Online learning platform.',
+  },
+  {
+    slug: 'duckduckgo',
+    url: 'https://duckduckgo.com',
+    description: 'Privacy-focused search engine.',
+  },
+];
