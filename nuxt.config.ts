82030a165f5fbe1712cbea7c5ba22491ab399c42
Pablo Cuevas <cuevasgpablo@gmail.com>
2024-02-22 15:23:55 -0300

    deps: vee-validate + yup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@unocss/nuxt', '@vueuse/nuxt', "nuxt-headlessui"],
+  modules: [
+    '@unocss/nuxt',
+    '@vueuse/nuxt',
+    'nuxt-headlessui',
+    '@vee-validate/nuxt',
+  ],
   css: ['@unocss/reset/tailwind.css', '~/assets/css/main.css'],
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
