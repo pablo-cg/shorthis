1a99c4dcb58a1df5406f0b0abf576619d4160f4a
Pablo Cuevas <cuevasgpablo@gmail.com>
2024-02-22 15:25:35 -0300

    rename: app-button moved to ui folder => UiButton

---
+++ components/ui/button.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button
+    class="inline-flex items-center gap-1 px-2 py-1 rounded-md text-white hover:text-light transition duration-100"
+    v-bind="$attrs"
+  >
+    <slot />
+  </button>
+</template>
