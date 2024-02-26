5f4a3957d30ba7373ff62b1bf9feffa4e54e06cb
Pablo Cuevas <cuevasgpablo@gmail.com>
2024-02-26 19:33:33 -0300

    add: template modal delete link

---
+++ components/modal/delete-link.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+interface Props {
+  isOpen: boolean;
+}
+
+interface Emits {
+  (e: 'close'): void;
+}
+
+defineProps<Props>();
+const emit = defineEmits<Emits>();
+
+function handleCloseModal() {
+  emit('close');
+}
+</script>
+
+<template>
+  <ModalWrapper
+    :is-open="isOpen"
+    @close="handleCloseModal"
+  >
+    test
+  </ModalWrapper>
+</template>
